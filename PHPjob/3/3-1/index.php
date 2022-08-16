5fff3ce21b022fbb5329f4244aa91d8973897c6b
makobluegreen <110874394+mkonoblue@users.noreply.github.com>
2022-08-16 15:06:35 +0900

    3章FizzBuzz問題
    提出

---
+++ PHPjob/3/3-1/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$num = "1";
+while($num < 101) {
+if($num%3 == 0 && $num%5 == 0){
+    echo "FizzBuzz!!";
+    echo "<br>";
+
+}elseif($num%3 == 0){
+    echo "Fizz!";
+    echo "<br>";
+
+}elseif($num%5 == 0){
+    echo "Buzz!";
+    echo "<br>";
+}else{
+    echo $num;
+    echo "<br>";
+}
+    $num++;
+
+}
