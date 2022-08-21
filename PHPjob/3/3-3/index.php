59d30469296b2c5e3d82bab71793b7b7cd8b2aac
makobluegreen <110874394+mkonoblue@users.noreply.github.com>
2022-08-21 12:51:47 +0900

    3章練習問題2
    3章練習問題2　提出

---
+++ PHPjob/3/3-3/index.php
@@ -0,0 +1,5 @@
+<form action="result.php" method="post">
+
+    ０〜９の番号を使って10桁以上の好きな数字の羅列を入力してください：<br>
+    <input type="text" name="inputNumber" />
+    <input type="submit" value="占う" />
---
+++ PHPjob/3/3-3/result.php
@@ -0,0 +1,36 @@
+<?php
+
+ 
+$number = $_POST['inputNumber'];
+$random = mt_rand(1,10);
+$uranai = substr($number,$random,1);
+
+switch ($uranai){
+    case "0":
+    print "凶です";
+    break;
+    
+    case "1":case "2":case "3":
+    print "小吉です";
+    break;
+
+    case "4":case "5":case "6":
+    print "中吉です";
+    break;    
+    
+    case "7":case "8":
+    print "吉です";
+    break;
+    
+    case "9":
+    print "大吉です";
+    break;
+
+    default:
+    print "半角で0～9までの数字の羅列を10桁以上入力してください";
+    break;    
+
+}
+echo "<br>";
+
+echo date("Y-m-d H:i:s",time());  
