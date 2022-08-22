ad1fd38fde9994298b4a874db03c5db016bb8912
makobluegreen <110874394+mkonoblue@users.noreply.github.com>
2022-08-22 10:46:21 +0900

    3章チェックテスト　完成
    3章チェックテスト　完成

--- PHPjob/3/3-4/answer.php
+++ PHPjob/3/3-4/answer.php
@@ -1,64 +1,68 @@
 <link rel="stylesheet" href="style.css">
-<?php 
+<?php
 //[question.php]から送られてきた名前の変数、
 //選択した回答、問題の答えの変数を作成
 
 $user_name = $_POST['user_name'];
-$s_port = $_POST['ans_port'];
-$v_web = $_POST['v_web'];
-$v_sql = $_POST['v_sql'];
+$s_port = $_POST['s_port'];
+$s_web = $_POST['s_web'];
+$s_sql = $_POST['s_sql'];
 
-echo $user_name;
-echo $port;
-echo $ans_port ;
 
 
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
-$ans_port = $port[0];
-$ans_web = $web[3];
-$ans_sql = $sql[1];
+$ans_port = $_POST['ans_port'];
+$ans_web = $_POST['ans_web'];
+$ans_sql = $_POST['ans_sql'];
 
+// echo $user_name;
+// echo $s_port;
+// echo $ans_port ;
+// echo $s_web ;
+// echo $ans_web ;
+// echo $s_sql ;
+// echo $ans_sql ;
 
 
 
 //選択した回答と正解が一致していれば「正解！」、
 //一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-function fun1($v_port,$ans_port){
-    if ($v_port == $ans_port){
+function fun1($s_port,$ans_port){
+    if ($s_port == $ans_port){
         echo "正解！";
     }else{
-        echo "不正解・・・";
+        echo "残念・・・";
     }
 }
 
-function fun2($v_web,$ans_web){
-    if ($v_web == $ans_web){
+function fun2($s_web,$ans_web){
+    if ($s_web == $ans_web){
         echo "正解！";
     }else{
-        echo "不正解・・・";
+        echo "残念・・・";
     }
 }
 
-function fun3($v_sql,$ans_sql){
-    if ($v_sql == $ans_sql){
+function fun3($s_sql,$ans_sql){
+    if ($s_sql == $ans_sql){
         echo "正解！";
     }else{
-        echo "不正解・・・";
+        echo "残念・・・";
     }
 }
 
 
 
 ?>
-<p><!--POST通信で送られてきた名前を表示-->さんの結果は・・・？</p>
+<p><!--POST通信で送られてきた名前を表示--><?php echo $user_name;?>さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php fun1() ?>
+<?php fun1($s_port,$ans_port) ?>
 
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php fun2() ?>
+<?php fun2($s_web,$ans_web) ?>
 
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php fun3() ?>
+<?php fun3($s_sql,$ans_sql) ?>
--- PHPjob/3/3-4/question.php
+++ PHPjob/3/3-4/question.php
@@ -2,6 +2,7 @@
 <?php
 //POST送信で送られてきた名前を受け取って変数を作成
 $user_name = $_POST['name'];
+
 //①画像を参考に問題文の選択肢の配列を作成してください。
 $port = [80,22,20,21];
 $web = ["PHP","Python","JAVA","HTML"];
@@ -20,9 +21,8 @@ $ans_sql = $sql[1];
 
 <form action="answer.php" method="post">
 
-<input type="text" name="user_name"
-    value="<?php echo $user_name; ?>" />
-    <?php echo $user_name; ?>
+    <input type="hidden" name="user_name"
+        value="<?php echo $user_name; ?>" />
 
 
 <h2>①HTTPのポート番号は何番？</h2>
@@ -30,27 +30,25 @@ $ans_sql = $sql[1];
 
 
     <?php foreach($port as $port_value){ ?>
-    <input type="radio" name="port"
-    value="<?php echo $port_value; ?>" />
+    <input type="radio" name="s_port"
+        value="<?php echo $port_value; ?>" />
     <?php echo $port_value; ?>
     <?php } ?>
-    <input type="hidden" name="ans_port" 
-    value="<?php echo $ans_port ?>" />
+    <input type="hidden" name="ans_port"
+        value="<?php echo $ans_port ?>" />
     <?php echo $ans_port ?>
-
 
 
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 
     <?php foreach($web as $web_value){ ?>
-    <input type="radio" name="v_web" 
-    value="<?php echo $web_value; ?>" />
+    <input type="radio" name="s_web"
+        value="<?php echo $web_value; ?>" />
     <?php echo $web_value; ?>
     <?php } ?>
-    <input type="hidden" name="ans_web" 
-    value="<?php echo $ans_web
- ?>">
+    <input type="hidden" name="ans_web"
+        value="<?php echo $ans_web ?>">
 
 
 
@@ -58,12 +56,12 @@ $ans_sql = $sql[1];
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 
     <?php foreach($sql as $sql_value){ ?>
-    <input type="radio" name="v_sql" 
-    value="<?php echo $sql_value; ?>" />
+    <input type="radio" name="s_sql"
+        value="<?php echo $sql_value; ?>" />
     <?php echo $sql_value; ?>
     <?php } ?>
-    <input type="hidden" name="ans_sql" 
-    value="<?php echo $ans_sql ?>">   
+    <input type="hidden" name="ans_sql"
+        value="<?php echo $ans_sql ?>">   
 
 
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
